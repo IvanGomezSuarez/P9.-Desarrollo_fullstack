c41c72f05e2895aa9bdca62b3dd717707a7eaceb
IvanGomezSuarez <59507021+IvanGomezSuarez@users.noreply.github.com>
2020-10-13 17:29:05 +0200

    [ramaIvanConExpress]

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,19 @@
-/* esto será para cuando usemos expressJS
+// esto será para cuando usemos expressJS
 const express = require('express'); // para crear un servidor
 const morgan = require('morgan');// muestra peticiones en el terminal
 const multer = require('multer'); // permite subir fotos a una ruta determinada
-const path = require('path');*/
+const path = require('path');
 
 // inicialiaciones con express
-//const app = express();
+const app = express();
 //require('./database'); //requerimos a la conexion con mongoDB
 
-//settings sin expres
-const http = require('http');
-const host = '127.0.0.1';
-const puerto= '7000';
-const server=http.createServer((request,response) => {
-  response.writeHead(200, {"Content-Type" : "text/plain"});
-  response.end('My APP');
-});
 
 // settings con express
-//app.set('port', 7000);
+app.set('port', 7000);
 
 // midlewares
-/*app.use(morgan('dev'));
+app.use(morgan('dev'));
 const storage = multer.diskStorage({ //define donde se colocan las imagenes y como se nombran
     destination: path.join(__dirname, 'public/uploads'),
     filename(req, file, cb){
@@ -31,18 +23,19 @@ const storage = multer.diskStorage({ //define donde se colocan las imagenes y co
 app.use(multer({storage}).single('image')); //subira una sola imagen al servidor
 app.use(express.urlencoded({extended: false})); //al rellenar formularios, crea un Json para luego ser leido facilmente
 app.use(express.json()); //para poder entender json en procesos AJAX
-*/
+
 //routes
-/*app.use('/api/players', require('./routes/players'));
+//app.use(require('./routes/index'));
+//app.use(require('./routes/login'));
+//app.use(require('./routes/profile'));
+app.use(require('./routes/players'));
+//app.use(require('./routes/register'));
 
 // static files
 app.use(express.static(path.join(__dirname, 'public'))); // dejamos acceso a la carpeta public desde el lado cliente
-*/
+
 //start server con express
-/*app.listen(app.get('port'), () =>{
+app.listen(app.get('port'), () =>{
     console.log('server on port', app.get('port'));
-})*/
-// sin express
-server.listen(puerto, host, () => {
-    console.log ('el servidor esta iniciado:' + host + ":" + puerto);
 })
+
