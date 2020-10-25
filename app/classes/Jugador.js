7680c671cbb8ebac1b0a20d7e1208fb5ffc12d31
cristianvia <cristianviar@gmail.com>
2020-10-25 20:52:29 +0100

    Create class jugador Create class partida Refactor login function in login.html Fix validationForm on register.html

---
+++ app/classes/Jugador.js
@@ -0,0 +1,14 @@
+class Jugador {
+    constructor(fullname, username, email, password, avatar){
+        this.fullname = fullname;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.avatar = avatar;
+    }
+
+    /*
+    NO TENEMOS CLARO SI LOS MÉTODOS DE REGISTRO Y AUTENTICACIÓN
+    TENDRÍAN QUE IR AQUÍ O EN OTRA CLASE 
+    */
+}
