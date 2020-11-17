e437fa448f32e20bc070a9c09a2898d3de193224
cristianvia <cristianviar@gmail.com>
2020-11-17 13:12:31 +0100

    First pug and express attempt

--- app/index.js
+++ app/index.js
@@ -1,57 +1,69 @@
-const logic = require ('./modules/GameLogic');
-const constructor = require('./modules/Constructors');
+// const logic = require ('./modules/GameLogic');
+// const constructor = require('./modules/Constructors');
 
-const url = require('url');
+// const url = require('url');
 
-const http = require("http");
-const fs = require("fs");
-const path = require("path");
-let arrancaServer = (puerto) => {
-    http.createServer((request, response)=> {
+// const http = require("http");
+// const fs = require("fs");
+// const path = require("path");
+// let arrancaServer = (puerto) => {
+//     http.createServer((request, response)=> {
         
-        let URLpath = request.url;
-        console.log(URLpath);
+//         let URLpath = request.url;
+//         console.log(URLpath);
 
-        //Enrutado partida
-        if(URLpath==='/partida'){
-            // response.end('LA NUEVA PARTIDA');
-            logic.winLogic();
-            constructor.players();
-        }
-        let filePath = request.url;
-        let encoding = 'UTF-8';
-        if (filePath == '/') {
-          filePath = '/login.html';
-        }
-        filePath = `${__dirname}/templates`+filePath;
-        fileExtension= path.extname(filePath);
-        switch (fileExtension) {
-            case ".css":
-                contentType = "text/css";
-            break;
-            case ".jpg":
-                contentType = "image/jpeg";
-                encoding = '';
-            break;
-            case ".js":
-                contentType = "text/javascript";
-            break;
-            case ".html":
-                contentType = "text/html";
-            default:
-                contentType = "text/html";
-        }
-        fs.readFile(filePath,{encoding:encoding}, (error,content)=>{
-            if(!error) {
-                response.writeHead(200, {"Content-Type": contentType});
-                response.write(content);
-                response.end();
-            } else {
-            response.writeHead(404, {"Content-Type": "text/html"});
-            response.write("error file");
-            response.end();
-            }
-        })
-    }).listen(puerto);
-};
-arrancaServer(8000);
+//         //Enrutado partida
+//         if(URLpath==='/partida'){
+//             // response.end('LA NUEVA PARTIDA');
+//             logic.winLogic();
+//             constructor.players();
+//         }
+//         let filePath = request.url;
+//         let encoding = 'UTF-8';
+//         if (filePath == '/') {
+//           filePath = '/login.html';
+//         }
+//         filePath = `${__dirname}/templates`+filePath;
+//         fileExtension= path.extname(filePath);
+//         switch (fileExtension) {
+//             case ".css":
+//                 contentType = "text/css";
+//             break;
+//             case ".jpg":
+//                 contentType = "image/jpeg";
+//                 encoding = '';
+//             break;
+//             case ".js":
+//                 contentType = "text/javascript";
+//             break;
+//             case ".html":
+//                 contentType = "text/html";
+//             default:
+//                 contentType = "text/html";
+//         }
+//         fs.readFile(filePath,{encoding:encoding}, (error,content)=>{
+//             if(!error) {
+//                 response.writeHead(200, {"Content-Type": contentType});
+//                 response.write(content);
+//                 response.end();
+//             } else {
+//             response.writeHead(404, {"Content-Type": "text/html"});
+//             response.write("error file");
+//             response.end();
+//             }
+//         })
+//     }).listen(puerto);
+// };
+// arrancaServer(8000);
+
+const express = require("express");
+const app = express();
+const port = 8000;
+
+app.set('view engine', 'pug');
+//const index = require('./templates/index.html')
+app.get('/', (req, res)=>{
+    res.render('login',{titulo: 'Login'})
+})
+
+app.listen(port, ()=> console.log("Listening on port" + port))
